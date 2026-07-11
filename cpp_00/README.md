c1148b19d486bb41657943c73c37f7a5040e45ae
mnieto-m <majanoyieto@gmail.com>
2026-07-11 17:40:52 +0200

    makefile okey in ex00

---
+++ cpp_00/README.md
@@ -0,0 +1,3 @@
+https://www.youtube.com/results?search_query=code+beauty+c%2B%2B+freecodecamp+
+https://www.youtube.com/watch?v=wN0x9eZLix4
+https://github.com/mxvements/ft_cpp00/blob/main/ex00/megaphone.cpp
--- cpp_00/ex01/src/contact.cpp
+++ cpp_00/ex01/src/contact.cpp
@@ -5,134 +5,86 @@
 #include <cstdlib> 
 #include <sstream>
 
-PhoneBook::PhoneBook()
-{
-	std::cout << "PhoneBook created: " << std::endl;
-	std::cout << "	Available commands:" << std::endl;
-	std::cout << "	> add		Save a new contact" << std::endl;
-	std::cout << "	> search	Display a specific contact" << std::endl;
-	std::cout << "	> exit		Quit and delete contacts" << std::endl;
 
-	for (int i = 0; i < 8; i++)
-	{
-		this->_contacts[i].reset();
-	}
+
+
+
+#include "Contact.hpp"
+#include <iostream>
+
+Contact::Contact(void){}
+Contact::~Contact(void){}
+
+/* setters */
+
+void Contact::set_index(std::string  index)
+{
+	this->_index = index;
 }
 
-PhoneBook::~PhoneBook(void) {}
-
-void PhoneBook::add(void)
+void Contact::set_first_name(std::string first_name)
 {
-	int 				i = 0;
-	std::stringstream	index;
-	std::string			first_name;
-	std::string			last_name;
-	std::string			nickname;
-	std::string			phone_number;
-	std::string			darkest_secret;
-
-	for (int j = 0; j < 8; j++)
-	{
-		if (this->_contacts[j].get_index() == "" || j == 7)
-		{
-			index << j;
-			i = j;
-			break;
-		}
-	}
-
-	std::cout << "  Save a new contact:" << std::endl;
-	std::cout << "> first_name:" << std::endl;
-	std::getline(std::cin, first_name);
-	std::cout << "> last_name:" << std::endl;
-	std::getline(std::cin, last_name);
-	std::cout << "> nickname:" << std::endl;
-	std::getline(std::cin, nickname);
-	std::cout << "> phone_number:" << std::endl;
-	std::getline(std::cin, phone_number);
-	std::cout << "> darkest_secret:" << std::endl;
-	std::getline(std::cin, darkest_secret);
-
-	this->_contacts[i].set_index(index.str());
-	this->_contacts[i].set_first_name(first_name);
-	this->_contacts[i].set_last_name(last_name);
-	this->_contacts[i].set_nickname(nickname);
-	this->_contacts[i].set_phone_number(phone_number);
-	this->_contacts[i].set_darkest_secret(darkest_secret);
-
-	std::cout << "  Contact saved!" << std::endl;
-	std::cout << "> Use another command" << std::endl;
+	this->_first_name = first_name;
 }
 
-static void truncate(const std::string &str, size_t max_len)
+void Contact::set_last_name(std::string last_name)
 {
-	if (str.length() > max_len)
-		std::cout << str.substr(0, max_len - 1).append(".");
-	else
-		std::cout << str;
+	this->_last_name =  last_name;
 }
 
-void PhoneBook::search(void)
+void Contact::set_nickname(std::string nickname)
 {
-	Contact 	contact;
-	std::string index;
-	int			i;
-
-	std::cout << "  List of contacts:" << std::endl;
-	for (int j = 0; j < 8; j++)
-	{
-		contact = this->_contacts[j];
-		std::cout << std::setiosflags(std::ios::right);
-		std::cout << std::setfill(' ') << std::setw(10);
-		std::cout << contact.get_index();
-		std::cout << " | ";
-		std::cout << std::setfill(' ') << std::setw(10);
-		truncate(contact.get_first_name(), 10);
-		std::cout << " | ";
-		std::cout << std::setfill(' ') << std::setw(10);
-		truncate(contact.get_last_name(), 10);
-		std::cout << " | ";
-		std::cout << std::setfill(' ') << std::setw(10);
-		truncate(contact.get_nickname(), 10);
-		std::cout << std::endl;
-	}
-	if (this->_contacts[0].get_index() == "")
-	{
-		std::cout << "  Empty list, returning" << std::endl;
-		std::cout << "> Use another command" << std::endl;
-		return ;
-	}
-
-	while (1)
-	{
-		std::cout << "> Type in the idx to show info" << std::endl;
-		std::cin >> index;
-		i = atoi(index.c_str());
-		if (i >= 0 && i < 8)
-		{
-			if (this->_contacts[i].get_index() == "")
-				std::cout << "  Contact empty, try again" << std::endl;
-			else
-				break ;
-		}
-		else
-			std::cout << "  Index out of bounds, try again" << std::endl;
-	}
-
-	std::cout << "  Contact info:" << std::endl;
-	std::cout << this->_contacts[i].get_first_name() << std::endl;
-	std::cout << this->_contacts[i].get_last_name() << std::endl;
-	std::cout << this->_contacts[i].get_nickname() << std::endl;
-	std::cout << this->_contacts[i].get_phone_number() << std::endl;
-	std::cout << this->_contacts[i].get_darkest_secret() << std::endl;
-	std::cout << "  End of contact" << std::endl;
-	std::cout << "> Use another command" << std::endl;
+	this->_nickname = nickname;
 }
 
-void PhoneBook::exit(void)
+void Contact::set_phone_number(std::string  phone_number)
 {
-	std::cout << "⠀⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⢀⠖⠢⡀⠀⠀⠀" << std::endl;
-	std::cout << "⠀⠀⠀⠀⠰⠊⠁⠀⠀⠀⠀⠀⠀⠈⠑⠢⣀⠀⠀⠀⠀⡞⠀⠘⠀⡆⠀⢠⠁⡠⠒⠢" << std::endl;
-	std::cout << "⠀⠀⣠⠂⠀⣠⣴⣶⡀⠀⠀⠀⠀⢠⣦⣄⠀⠣⡀⠀⠀⢡⠀⠀⡀⠇⠀⠇⠰⠀⢠⠊" << std::endl;
-std::cout << "	⠀⠀⠀⠀⠀⢀⡀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣀⡀⠀⢀⠖⠢⡀⠀⠀⠀\n⠀⠀⠀⠀⠰⠊⠁⠀⠀⠀⠀⠀⠀⠈⠑⠢⣀⠀⠀⠀⠀⡞⠀⠘⠀⡆⠀⢠⠁⡠⠒⠢\n⠀⠀⣠⠂⠀⣠⣴⣶⡀⠀⠀⠀⠀⢠⣦⣄⠀⠣⡀⠀⠀⢡⠀⠀⡀⠇⠀⠇⠰⠀⢠⠊\n⠀⡰⠃⠀⠀⢿⣿⠿⠁⠀⠀⠀⠀⠈⠻⢿⠗⠀⠱⡀⠀⠈⢆⠀⠀⠂⠀⠈⠁⠀⡆⠀\n⠰⠁⡇⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⢡⠀⠄⠈⠄⠀⠀⠀⠀⠀⠀⠀⠀\n⢈⠀⢣⣄⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⡆⠘⢢⣀⡀⠀⣀⠀⠀⠀⠀⢠⠆⠀\n⢸⠀⠘⣿⣦⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣰⣿⠃⠸⠀⠀⠀⠀⠀⠐⠤⠤⠂⠁⠀⠀\n⠀⢧⡀⠙⢿⣷⣄⠀⠀⠀⠀⠀⠀⠀⢀⣼⡿⠃⢠⠂⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠈⢿⠀⠈⠻⣿⣷⣦⣄⣀⣀⣤⣾⡿⠋⠀⣠⠃⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠑⢄⠀⠀⠀⠉⠙⠉⠉⠉⠁⠀⡴⠋⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀\n⠀⠀⠀⠀⠀⠀⠒⠚⠲⠶⠶⠶⠾⠚⠉⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀" << std::endl;
+	this->_phone_number = phone_number;
+}
+
+void Contact::set_darkest_secret(std::string darkest_secret)
+{
+	this->_darkest_secret = darkest_secret;
+}
+
+/* getters */
+
+std::string  Contact::get_index(void)
+{
+	return this->_index;
+}
+
+std::string Contact::get_first_name(void)
+{
+	return this->_first_name;
+}
+
+std::string Contact::get_last_name(void)
+{
+	return this->_last_name;
+}
+
+std::string Contact::get_nickname(void)
+{
+	return this->_nickname;
+}
+
+std::string Contact::get_phone_number(void)
+{
+	return this->_phone_number;
+}
+
+std::string Contact::get_darkest_secret(void)
+{
+	return this->_darkest_secret;
+}
+
+void Contact::reset(void)
+{
+	this->set_index("");
+	this->set_first_name("");
+	this->set_last_name("");
+	this->set_nickname("");
+	this->set_phone_number("");
+	this->set_darkest_secret("");
 }
